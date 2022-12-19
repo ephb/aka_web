57634e762996b84c71607ceecad9e4ee6da4030d
Philipp Bontemps <philipp@vrtxlabs.com>
2022-12-19 16:57:35 +0100

    Add rules to reserve, change styling, change "Protokolle"

--- reserve/index.php
+++ reserve/index.php
@@ -14,13 +14,18 @@ include('../a_common_scripts//sec.php');
 echo '<table border="0" width="100%"><tr><td width="5%">&nbsp;</td><td width="90%"  class="head">';
 #tab_go("100%",250,'center','');
 echo
-    '<div style="float:right;">
+    '<div style="float:right;"><a href="index.php?'.SID.'" class="head">&Uuml;bersicht</a> &nbsp; | &nbsp;
+    <a href="index.php?'.SID.'&mod=rules" class="head">Regeln</a> &nbsp; | &nbsp;
 <a href="index.php?logout=1&' . $SID . '" class="head">Logout</a> &nbsp;</div></td><td width="5%">&nbsp;</td></tr><tr><td>&nbsp;</td><td>';
 ########### menü ###############
 ########### reinladen ###############
+if(isset($_GET['mod'])){
+if($_GET['mod']=='rules') {	include('rules.php'); }
+}
 include('tab.php');
 ########### reinladen ###############
 echo '</td><td>&nbsp;</td></tr></table><center>';
+
 include("counter.php");
 echo impressum() . '</center></body></html>';
 ?>
---
+++ reserve/rules.php
@@ -0,0 +1,11 @@
+<?php
+echo'
+1. Grube links: 6 Monate<br>
+2. Grube rechts: 2 Wochen<br>
+3. Mehrzweckarbeitsplatz: 2 Wochen<br>
+4. Linke Bühne: 6 Monate<br>
+5. Rechte Bühne: 3 Tage<br>
+6. Emporen: 28 Tage<br>
+7. Trailer: Kontakt mit Lars aufnehmen<br><br><br>
+'
+?>
