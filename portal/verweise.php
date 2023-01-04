ec2e82ad30c03e01efb8aa065d472e833bb04cef
Philipp Bontemps <philipp@vrtxlabs.com>
2023-01-04 23:13:01 +0100

    Hide "Liste des Tyrannen"

--- portal/verweise.php
+++ portal/verweise.php
@@ -9,8 +9,8 @@ echo'
 <div style="float:left;" class="head">
 <a target="Daten" href="../reserve/" class="head">Reservierungssystem</a> &nbsp; | &nbsp;
 <a target="Daten" href="../drinks/" class="head">Getr&auml;nkeabrechnung</a> &nbsp; | &nbsp;
-<a target="_blank" href="https://my.hidrive.com/share/zf11qosqq0" class="head">Protokolle</a> &nbsp; | &nbsp;
-<a target="Daten" href="../liste_des_tyrannen/" class="head">Liste des Tyrannen</a> &nbsp; | &nbsp;
+<a target="_blank" href="https://my.hidrive.com/share/zf11qosqq0" class="head">Protokolle/Dateien</a> &nbsp; | &nbsp;
+<!-- <a target="Daten" href="../liste_des_tyrannen/" class="head">Liste des Tyrannen</a> &nbsp; | &nbsp; -->
 <a target="Daten" href="startseite.php" class="head">Start</a>
 </div><div style="float:right;"></div><br>
 <hr style="height:0;  border-bottom:1px dotted #000000; border-top: 0px;">
